5b39726b1a71a28e008022d2b22cf5e5141423e4
RITWIZ SINHA <https://github.com/ritwizsinha>
2019-01-12 18:37:42 +0530

    Made the pages dynamic

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from realtors.models import Realtor
+from listings.models import Listing
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
+    context ={
+        'listings' : listings
+    }
+
+    return render(request, 'pages/index.html',context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors =Realtor.objects.all()
+
+    context = {
+        'realtors' : realtors
+    }
+    return render(request, 'pages/about.html',context)
